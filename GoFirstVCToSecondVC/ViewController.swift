332c5dc2621c628b9cad716ededb0f5078109fa5
Wdp <wdpmobileteam@gmail.com>
2020-08-02 18:42:27 +0530

    Initial Commit

---
+++ GoFirstVCToSecondVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoFirstVCToSecondVC
+//
+//  Created by WDP on 02/08/20.
+//  Copyright © 2020 WDP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
